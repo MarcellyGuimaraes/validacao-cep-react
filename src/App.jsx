64ab05ecb27de226da2b1c1b45bbb088f218a78f
IsisG13 <isissouzaguimaraes@gmail.com>
2022-12-21 17:03:28 -0300

    arrumando a lógica

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,52 @@
 import axios from 'axios'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
+import api from './api'
 
 function App() {
-  const [cep, setCep] = useState(null)
   const [searchQuery, setSearchQuery] = useState('')
-  const [filteredList, setFilteredList] = useState(null)
+  const [cep, setCep] = useState({})
 
-  const handleChange = (event) => {
-    const query = event.target.value
-    setSearchQuery(query)
-  }
-  const handleSearch = () => {
-    const searchList = axios
-      .get(`https://viacep.com.br/ws/${searchQuery}/json/`)
-      .then((response) => {
-        setCep(response.data)
-      })
-      .catch((error) => {
-        console.log('erro', error)
-      })
+  async function handleSearch() {
+    if (searchQuery === '') {
+      return
+    }
 
-    setFilteredList(searchList)
+    try {
+      const response = await api.get(`/${searchQuery}/json`)
+      if (Object.keys(response.data).length === 1) {
+        alert('Esse cep não existe, verifique e tente novamente :(')
+      }
+      setCep(response.data)
+      setSearchQuery('')
+    } catch {
+      alert('Digite um cep válido!')
+      setSearchQuery('')
+    }
   }
 
   return (
-    <div className="m-4">
-      <input
-        className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        type="text"
-        placeholder="Search"
-        value={searchQuery}
-        onChange={handleChange}
-      />
-      <button onClick={handleSearch} className="p-1 bg-red-200">
-        teste
-      </button>
+    <div className="m-4 flex flex-col">
+      <div className="flex">
+        <input
+          className="block w-3/4 p-4 pl-10 text-sm text-gray-900 border border-gray-300 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          type="text"
+          placeholder="Search"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button onClick={handleSearch} className=" w-1/4 bg-red-200">
+          pesquisar
+        </button>
+      </div>
 
-      {cep ? console.log(cep) : ''}
+      {Object.keys(cep).length > 1 && (
+        <div>
+          <p>Cep: {cep.cep}</p>
+          <p>Estado: {cep.uf}</p>
+          <p>Cidade: {cep.localidade}</p>
+          <p>Logradouro: {cep.logradouro}</p>
+        </div>
+      )}
     </div>
   )
 }
